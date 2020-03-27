5d100fb89ad80d8878fd67d7e8317096a62c9e0d
Brad Smith <brad@smithwebtek.com>
2020-03-27 10:47:10 -0400

    Refactor to use yml for env variables

--- lib/twilio_capability.rb
+++ lib/twilio_capability.rb
@@ -4,9 +4,14 @@ class TwilioCapability
   def self.generate(role)
     # To find TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN visit
     # https://www.twilio.com/user/account
-    account_sid     = ENV['TWILIO_ACCOUNT_SID']
-    auth_token      = ENV['TWILIO_AUTH_TOKEN']
-    application_sid = ENV['TWIML_APPLICATION_SID']
+    # account_sid     = ENV['TWILIO_ACCOUNT_SID']
+    account_sid = Rails.application.secrets.twilio[:account_sid]
+    
+    # auth_token      = ENV['TWILIO_AUTH_TOKEN']
+    auth_token = Rails.application.secrets.twilio[:auth_token]
+
+    # application_sid = ENV['TWIML_APPLICATION_SID']
+    application_sid = Rails.application.secrets.twilio[:sid]
  
     capability = Twilio::JWT::ClientCapability.new(account_sid, auth_token)
     outgoing_scope = Twilio::JWT::ClientCapability::OutgoingClientScope.new(application_sid, role)
