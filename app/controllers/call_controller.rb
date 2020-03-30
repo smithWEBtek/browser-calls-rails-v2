552b1b6b492484ddd8fa7f579629f832577f1399
Brad Smith <brad@smithwebtek.com>
2020-03-30 09:38:45 -0400

    Add parens in method

--- app/controllers/call_controller.rb
+++ app/controllers/call_controller.rb
@@ -10,7 +10,7 @@ class CallController < ApplicationController
   def twilio_reponse
     twilio_number = ENV['TWILIO_PHONE_NUMBER']
     res = Twilio::TwiML::VoiceResponse.new do |response|
-      dial = Twilio::TwiML::Dial.new caller_id: twilio_number
+      dial = Twilio::TwiML::Dial.new(caller_id: twilio_number)
 
       if params.include?(:phoneNumber)
         dial.number params[:phoneNumber]
