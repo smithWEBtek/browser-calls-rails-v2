9cf9220ff2145799ab7106e0980748f789b86385
Brad Smith <brad@smithwebtek.com>
2020-03-30 12:26:28 -0400

    Add 6034944147 as callerId verified number

--- app/assets/javascripts/browser-calls.js
+++ app/assets/javascripts/browser-calls.js
@@ -92,7 +92,7 @@ Twilio.Device.incoming(function (connection) {
 function callCustomer(phoneNumber) {
   updateCallStatus("Calling " + phoneNumber + "...");
 
-  var params = { "phoneNumber": phoneNumber, "callerId": "6173973501" };
+  var params = { "phoneNumber": phoneNumber, "callerId": "6034944147" };
   console.log('callCustomerParams: ', params)
   Twilio.Device.connect(params);
 }
