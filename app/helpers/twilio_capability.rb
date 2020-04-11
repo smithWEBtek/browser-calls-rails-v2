c0e8deeccd5cd71ce5e35bce4bb76c321a9a574a
github username <github email address>
2020-04-11 16:15:52 -0400

    Switch to secrets.yml

--- app/helpers/twilio_capability.rb
+++ app/helpers/twilio_capability.rb
@@ -1,15 +1,16 @@
 class TwilioCapability
   def self.generate(role)
 
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token = ENV['TWILIO_AUTH_TOKEN']
-    application_sid = ENV['TWIML_APPLICATION_SID']
-    # application_sid = Rails.application.secrets.twilio[:sid]
+    
+    account_sid = Rails.application.secrets.twilio[:account_sid]
+    auth_token = Rails.application.secrets.twilio[:auth_token]
+    application_sid = Rails.application.secrets.twilio[:twiml_application_sid]
+    #application_sid = Rails.application.secrets.twilio[:sid]
+    
+    # account_sid = ENV['TWILIO_ACCOUNT_SID']
+    # auth_token = ENV['TWILIO_AUTH_TOKEN']
+    # application_sid = ENV['TWIML_APPLICATION_SID']
 
-    # account_sid = Rails.application.secrets.twilio[:account_sid] || ENV['TWILIO_ACCOUNT_SID']
-    # auth_token = Rails.application.secrets.twilio[:auth_token] || ENV['TWILIO_AUTH_TOKEN']
-    # application_sid = Rails.application.secrets.twilio[:twiml_application_sid] || ENV['TWIML_APPLICATION_SID']
-  
     capability = Twilio::JWT::ClientCapability.new(account_sid, auth_token)
     outgoing_scope = Twilio::JWT::ClientCapability::OutgoingClientScope.new(application_sid, role)
     capability.add_scope outgoing_scope
