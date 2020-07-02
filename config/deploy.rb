f50b941aa45bef5540c37bd535ab4f4628f01499
Brad Smith <brad@smithwebtek.com>
2020-07-02 16:43:16 -0400

    Try deploying via qa

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,8 @@ set :repo_url, "git@github.com:smithwebtek/browser-calls-rails-v2.git"
 # set :branch, "master"
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deployer/browsercalls"
+# set :deploy_to, "/home/deployer/browsercalls"
+set :deploy_to, "/home/deployer/harvard_bwel"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
